9234929870a3d13cbf26e845d1e43f475a9bcda0
Carleton Cabarrus <carleton.cabarrus@onshoreoutsourcing.com>
2019-09-16 15:32:26 -0500

    angular app for adventure works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Optional, SkipSelf } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Configuration } from '../configuration';
+import { HttpClient } from '@angular/common/http';
+
+
+import { AddressesService } from '../api/addresses.service';
+import { CustomerAddressesService } from '../api/customerAddresses.service';
+import { CustomersService } from '../api/customers.service';
+import { ErrorLogsService } from '../api/errorLogs.service';
+import { ProductCategoriesService } from '../api/productCategories.service';
+import { ProductDescriptionsService } from '../api/productDescriptions.service';
+import { ProductModelProductDescriptionsService } from '../api/productModelProductDescriptions.service';
+import { ProductModelsService } from '../api/productModels.service';
+import { ProductsService } from '../api/products.service';
+import { SalesOrderDetailsService } from '../api/salesOrderDetails.service';
+import { SalesOrderHeadersService } from '../api/salesOrderHeaders.service';
+import { ValuesService } from '../api/values.service';
 
 @NgModule({
   declarations: [
@@ -12,7 +28,38 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    AddressesService,
+    CustomerAddressesService,
+    CustomersService,
+    ErrorLogsService,
+    ProductCategoriesService,
+    ProductDescriptionsService,
+    ProductModelProductDescriptionsService,
+    ProductModelsService,
+    ProductsService,
+    SalesOrderDetailsService,
+    SalesOrderHeadersService,
+    ValuesService
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  public static forRoot(configurationFactory: () => Configuration): ModuleWithProviders {
+    return {
+        ngModule: AppModule,
+        providers: [ { provide: Configuration, useFactory: configurationFactory } ]
+    };
+}
+
+constructor( @Optional() @SkipSelf() parentModule: AppModule,
+             @Optional() http: HttpClient) {
+    if (parentModule) {
+        throw new Error('ApiModule is already loaded. Import in your base AppModule only.');
+    }
+    if (!http) {
+        throw new Error('You need to import the HttpClientModule in your AppModule! \n' +
+        'See also https://github.com/angular/angular/issues/20575');
+    }
+}
+}
